7fc86b2eb65fde026e9b5022b9de3b2da7140a1f
gabor.szell <500gabor@gmail.com>
2025-02-23 22:08:02 +0100

    Added main.py as a CLI application.

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-from src import (read_database, read_binary_file, check_missing_database_data,
-                 filter_invalid_timestamps, filter_gps_errors)
-from src.utils import MissingIDTracker
+import argparse
+from src import (read_database, read_binary_file, check_missing_database_data, filter_invalid_timestamps,
+                 filter_gps_errors, interpolate_missing_records, visualize_map, detect_roundabouts)
 
+def main():
+    parser = argparse.ArgumentParser(description="Process GPS binary alongside database data and detect roundabouts.")
 
-packets = read_database(r"C:\work\aimotive\sensor_index.db")
-check_missing_database_data(packets)
-gdf = read_binary_file(r"C:\work\aimotive\gps_data.bin", packets)
-filter_invalid_timestamps(gdf)
-filter_gps_errors(gdf)
+    parser.add_argument("--db_path", required=True, help="Path to the sensor index database")
+    parser.add_argument("--binary_path", required=True, help="Path to the binary GPS data file.")
+    parser.add_argument("--visualize", action="store_true", help="Visualize the processed GPS data on a (folium) map")
 
-missing_id_tracker = MissingIDTracker()
-print(missing_id_tracker.get_missing_ids())
-gdf.to_csv("asd.csv")
+    args = parser.parse_args()
 
-print("Stop")
+    print("Parsing database...")
+    packets = read_database(args.db_path)
+    print("Checking for missing database data based on record_ids...")
+    check_missing_database_data(packets)
+
+    print("Parsing the binary data...")
+    df = read_binary_file(args.binary_path, packets)
+
+    print("Filtering invalid timestamps...")
+    df = filter_invalid_timestamps(df)
+
+    print("Filtering GPS errors...")
+    df = filter_gps_errors(df)
+
+    print("Interpolating missing records...")
+    df = interpolate_missing_records(df)
+
+    print("Detecting roundabouts...")
+    roundabouts_dataframe = detect_roundabouts(df)
+
+    if args.visualize:
+        print("Visualizing map...")
+        visualize_map(df)
+    else:
+        print("Map visualization is disabled. Use --visualize argument to enable it.")
+
+    print("Done. For further information check the logs folder!")
+
+if __name__ == "__main__":
+    main()
